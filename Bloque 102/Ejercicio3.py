4111c4851d00c411956bd5e4a646cd7d30ad4653
carlos Carreira <a25carloscg@iesantonlosada.gal>
2026-09-22 14:11:06 +0200

    B102 ej3

---
+++ Bloque 102/Ejercicio3.py
@@ -0,0 +1,9 @@
+#pedir nombre y apellidos.
+
+nombre = (input("Introducir nombre:"))
+apellido1 = (input("Introducir apellido1: "))
+apellido2 = (input("Introducir apellido2: "))
+
+#mostrar mensaje.
+
+print(f"El usuario {apellido1} {apellido2}, {nombre} se ha registrado correctamente")
