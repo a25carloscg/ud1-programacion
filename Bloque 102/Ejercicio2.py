97a4cc884996627464927d30eba4ae1847c3f8fd
carlos Carreira <a25carloscg@iesantonlosada.gal>
2026-09-22 14:10:35 +0200

    B102 ej2

---
+++ Bloque 102/Ejercicio2.py
@@ -0,0 +1,9 @@
+#pedir 2 números enteros
+num1 = int(input("introducir el primer número: "))
+num2 = int(input("introducir el segundo número: "))
+
+# mostrar división "/" división entera "//" y resto de la división "%"
+
+print("división real: ", num1 / num2 )
+print("división entera: ", num1 // num2)
+print("resto: ", num1 % num2)
