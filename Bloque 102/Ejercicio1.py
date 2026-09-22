4ec04fbb67b268cf08548db1a1eea4a389d79705
carlos Carreira <a25carloscg@iesantonlosada.gal>
2026-09-22 14:09:39 +0200

    B102 ej1

---
+++ Bloque 102/Ejercicio1.py
@@ -0,0 +1,8 @@
+# Pedir dos números enteros al usuario
+numero1 = int(input("Introduce el primer número: "))
+numero2 = int(input("Introduce el segundo número: "))
+
+# Mostrar la suma, resta y producto
+print("Suma:", numero1 + numero2)
+print("Resta:", numero1 - numero2)
+print("Producto:", numero1 * numero2)
